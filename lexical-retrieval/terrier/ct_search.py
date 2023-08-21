83e14bb49100d0fc195cc076056c41e5584d9c49
Maximilian Seeliger <maximilian.seeliger@gmail.com>
2023-08-22 01:07:46 +0200

    Refactoring and improved result printouts

--- lexical-retrieval/terrier/ct_search.py
+++ lexical-retrieval/terrier/ct_search.py
@@ -2,43 +2,70 @@ import pyterrier as pt
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-pt.init()
+TOPICS_FILE = "data/patients/topics2023.xml"
 
-index = pt.IndexFactory.of("./ct_index")
+def create_query_dataframe():
+    tree = ET.parse(TOPICS_FILE)
+    root = tree.getroot()
 
-# Parse the topics file to get the queries
-file_path = "data/patients/topics2023.xml"
-tree = ET.parse(file_path)
-root = tree.getroot()
+    # Create a query dataframe for every topic
+    queries = []
+    for topic in root:
+        # Print number with all the attributes
+        patient_tokens = [topic.attrib["template"]]
+        for child in topic:
+            patient_tokens.append(child.attrib["name"])
+            if child.text is not None:
+                patient_tokens.append(child.text.replace("/", "-"))
+        patient_query = " ".join(patient_tokens)
+        queries.append({"query": patient_query, "qid": topic.attrib["number"]})
 
-# Create a query dataframe for every topic
-queries = []
-for topic in root:
-    # Print number with all the attributes
-    patient_tokens = []
-    for child in topic:
-        patient_tokens.append(child.attrib["name"])
-        if child.text is not None:
-            patient_tokens.append(child.text.replace("/", "-"))
-    patient_query = " ".join(patient_tokens)
-    queries.append({"query": patient_query, "qid": topic.attrib["number"]})
+    # Create a dataframe from the queries
+    queries = pd.DataFrame(queries)
 
-# Create a dataframe from the queries
-queries = pd.DataFrame(queries)
-
-print(queries.info())
+    print(queries.info())
+    return queries
 
 
-# Create a retrieval pipeline
-pipeline = pt.BatchRetrieve(index, wmodel="BM25")
+def rank_documents(index, queries, num_results_per_query=None):
 
-results = pipeline.transform(queries)
+    # Create a retrieval pipeline
+    pipeline = pt.BatchRetrieve(index, wmodel="BM25", verbose=True) 
+    
+    if num_results_per_query is not None:
+        pipeline = pipeline % num_results_per_query
 
-# Print the first 10 results
-print(results)
+    pipeline.compile()
+    results = pipeline.transform(queries) 
 
-# Retrieve the first result
-print(results.iloc[0])
+    print(results.info())
+    return results
 
-# Retrieve the first result's document
-print(index.getMetaIndex().getAllItems(results.iloc[0]["docid"]))
+
+
+def print_results(index, queries, results):
+
+    for _, query in queries.iterrows():
+        print(f"Query: {query['query']}, Number: {query['qid']}")
+        print("Results:")
+        # Print the top 10 results (highest score) for this query (qid = query["qid"])
+        for j, result in results[results["qid"] == query["qid"]].head(10).iterrows():
+            print(f"Trial: {result['docno']}, Score: {result['score']}")
+            # print(index.getMetaIndex().getAllItems(result["docid"]))
+            print(index.getMetaIndex().getItem("brief_title", result["docid"]))
+            print()
+            
+        print()
+
+
+if __name__ == "__main__":
+
+    queries = create_query_dataframe()
+
+
+    pt.init()
+    index = pt.IndexFactory.of("./ct_index")
+    results = rank_documents(index, queries, 100)
+
+    print_results(index, queries, results)
+
--- lexical-retrieval/terrier/index_setup.py
+++ lexical-retrieval/terrier/index_setup.py
@@ -1,36 +1,46 @@
 import pyterrier as pt
+from tqdm import tqdm
 from dataclasses import asdict
 
 from CTnlp.parsers import parse_clinical_trials_from_folder
 
 TRIALS_FOLDER = "data/clinical_trials"
 
-cts = parse_clinical_trials_from_folder(folder_name=TRIALS_FOLDER)
-cts = [asdict(ct) for ct in cts]
+def get_clinical_trial_dict(first_n=None):
+    cts = parse_clinical_trials_from_folder(folder_name=TRIALS_FOLDER, first_n=first_n)
+    cts = [asdict(ct) for ct in cts]
+    return cts
 
-# Rename field nct_id to docno (required by pyterrier)
-for ct in cts:
-    ct["docno"] = ct.pop("nct_id")
 
-# Relevant fields:
-ct_fields = ["docno", "brief_title", "brief_summary",
-             "detailed_description", "inclusion", "exclusion",
-             "gender", "minimum_age", "maximum_age",
-             "conditions", "interventions"] 
-print("Fields:", ct_fields)
+def index_clinical_trials(cts, ct_meta_fields, ct_text_fields):
+    pt.init()
+    iter_indexer = pt.IterDictIndexer("./ct_index", overwrite=True, verbose=True, num_docs=len(cts),
+                                      meta=ct_meta_fields,
+                                      stemmer="porter",
+                                      stopwords="terrier",
+                                      tokeniser="english")
 
-# Create a new index
-pt.init()
-iter_indexer = pt.IterDictIndexer("./ct_index", overwrite=True, verbose=True,
-                                  meta=ct_fields,
-                                  stemmer="porter",
-                                  stopwords="terrier",
-                                  tokeniser="english")
+    print("Indexing...")
+    indexref = iter_indexer.index(cts, fields=ct_text_fields)
 
-print("Indexing...")
-# Index the documents
-indexref = iter_indexer.index(cts, fields=ct_fields[1:])
+    index = pt.IndexFactory.of(indexref)
+    print(index.getCollectionStatistics().toString())
 
-index = pt.IndexFactory.of(indexref)
+if __name__ == "__main__":
+    
+    cts = get_clinical_trial_dict()
 
-print(index.getCollectionStatistics().toString())
+    # Rename field nct_id to docno (required by pyterrier)
+    for ct in tqdm(cts):
+        ct["docno"] = ct.pop("nct_id")
+
+    # Relevant fields:
+    ct_fields = ["docno", "brief_title", "brief_summary",
+                 "detailed_description", "inclusion", "exclusion",
+                 "gender", "minimum_age", "maximum_age",
+                 "conditions", "interventions"] 
+    print("Fields:", ct_fields, ct_fields[1:])
+
+    index_clinical_trials(cts, ct_fields, ct_fields[1:])
+
+

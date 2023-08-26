50888c2f10642a09cedc07df68013cc3e12c15ab
Maximilian Seeliger <maximilian.seeliger@gmail.com>
2023-08-27 00:45:21 +0200

    Generalized index creation

--- lexical-retrieval/terrier/index.py
+++ lexical-retrieval/terrier/index.py
@@ -1,32 +1,59 @@
 from argparse import ArgumentParser
-
+import os
 import pyterrier as pt
-from tqdm import tqdm
+pt.init()
+
+import pandas as pd
 from dataclasses import asdict
 
 from CTnlp.parsers import parse_clinical_trials_from_folder
 
-TRIALS_FOLDER = "data/clinical_trials"
+INDICES_FOLDER = "./indices/"
 
-def get_clinical_trial_dict(first_n=None):
-    cts = parse_clinical_trials_from_folder(folder_name=TRIALS_FOLDER, first_n=first_n)
-    cts = [asdict(ct) for ct in cts]
-    return cts
+def setup(df, index_name, meta_fields=["docno"], force=False):
 
+    if exists(index_name) and not force:
+        return ValueError(f"Index {index_name} already exists")
 
-def index_clinical_trials(cts, ct_meta_fields, ct_text_fields):
-    pt.init()
-    iter_indexer = pt.IterDictIndexer("./ct_index", verbose=True, num_docs=len(cts),
-                                      meta=ct_meta_fields,
-                                      stemmer="porter",
-                                      stopwords="terrier",
-                                      tokeniser="english")
+    indexer = pt.DFIndexer(index_path(index_name),
+                           stemmer="porter",
+                           stopwords="terrier",
+                           tokeniser="english",
+                           verbose=True,
+                           overwrite=force)
 
-    print("Indexing...")
-    indexref = iter_indexer.index(cts, fields=ct_text_fields)
+    index_ref = indexer.index(df["text"], df[meta_fields])
+    index = pt.IndexFactory.of(index_ref)
 
-    index = pt.IndexFactory.of(indexref)
     print(index.getCollectionStatistics().toString())
+
+    return index
+
+
+
+def exists(index_name):
+
+    try:
+        pt.IndexFactory.of(index_path(index_name))
+        return True
+    except:
+        return False
+    
+def get(index_name):
+
+    if not exists(index_name):
+        raise ValueError(f"Index {index_name} does not exist")
+    
+    return pt.IndexFactory.of(index_path(index_name))
+
+def index_path(index_name):
+
+    path = INDICES_FOLDER + index_name
+    # check for path traversal
+    if not os.path.abspath(path).startswith(os.path.abspath(INDICES_FOLDER)):
+        raise ValueError(f"Invalid index name: {index_name}")
+    
+    return path
 
 if __name__ == "__main__":
 
@@ -34,23 +61,28 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--first_n", type=int, default=None, help="Number of clinical trials to index")
     args = parser.parse_args()
 
-    cts = get_clinical_trial_dict(args.first_n)
+    cts = parse_clinical_trials_from_folder(folder_name="data/clinical_trials", first_n=args.first_n)
+    cts = [asdict(ct) for ct in cts]
 
-    # Rename field nct_id to docno (required by pyterrier)
-    for ct in tqdm(cts):
-        ct["docno"] = ct.pop("nct_id")
+    
 
     # Relevant fields:
     ct_fields = ["brief_title", "brief_summary",
-                 "detailed_description", "inclusion", "exclusion",
-                 "gender", "minimum_age", "maximum_age",
-                 "conditions", "interventions"] 
-    
-    meta_fields = {
-        "docno": 20,
-        "brief_title": 512,
-    }
+                 "detailed_description", "inclusion", "exclusion"]
 
-    index_clinical_trials(cts, meta_fields, ct_fields)
+    df = pd.DataFrame(cts)
+    print(df.info())
+
+    # convert inclusion and exclusion lists to strings (space separated)
+    df["inclusion"] = df["inclusion"].apply(lambda x: " ".join(x))
+    df["exclusion"] = df["exclusion"].apply(lambda x: " ".join(x))
+
+    df["docno"] = df.pop("nct_id")
+    df["text"] = df[ct_fields].apply(lambda x: " ".join(x), axis=1)
+    df = df[["docno", "text", "brief_title"]]
+
+    setup(df, "ct_base", ["docno", "brief_title"], force=True)
+
+    
 
 

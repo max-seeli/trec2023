26d1975d6b6b70b2cb522c58a29bd490221255e0
Maximilian Seeliger <maximilian.seeliger@gmail.com>
2023-09-02 14:53:48 +0200

    Refactoring for readability

--- preprocessing/preprocess_trials.py
+++ preprocessing/preprocess_trials.py
@@ -2,74 +2,51 @@ from text_normalization import pipeline
 import json
 from tqdm import tqdm
 
+TRIALS_SOURCE = "data/clinical_trials/trial_pre_selection_16k.json"
+PREPROCESSED_TRIALS_TARGET = "data/clinical_trials/trials_preprocessed.json"
+
 trials = []
 
-with open("data/clinical_trials/trial_pre_selection_16k_slim.json", "r") as json_file:
+with open(TRIALS_SOURCE, "r") as json_file:
     trials = json.load(json_file)
 
-with open("trials_preprocessed.json", "w") as file:
-    file.write("[")
+trials = trials[:10]
 
-for index, trial in tqdm(enumerate(trials)):
+preprocessed_trials = []
+for index, trial in tqdm(enumerate(trials), total=len(trials)):
+    
     inclusion_criteria = trial['inclusion']
+    inclusion_entities = [inclusion_entity for inclusion_criterium in inclusion_criteria for inclusion_entity in pipeline(inclusion_criterium)]
     
-    inclusion_criteria_preprocessed = []
-    for criterium in inclusion_criteria:
-        preprocessed_criteria = pipeline(criterium)
-        for preprocessed_criterium in preprocessed_criteria:
-            inclusion_criteria_preprocessed.append(preprocessed_criterium)
-
     exclusion_criteria = trial['exclusion']
+    exclusion_entities = [exclusion_entity for exclusion_criterium in exclusion_criteria for exclusion_entity in pipeline(exclusion_criterium)]
+
+    # Move negated entities to other list
+    inclusion_entities_negated = [entity for entity in inclusion_entities if entity.startswith("n_")]
+    exclusion_entities_negated = [entity for entity in exclusion_entities if entity.startswith("n_")]
+
+    inclusion_entities = [entity for entity in inclusion_entities if entity not in inclusion_entities_negated]
+    inclusion_entities.extend([entity[2:] for entity in exclusion_entities_negated])
     
-    exclusion_criteria_preprocessed = []
-    for criterium in exclusion_criteria:
-        preprocessed_criteria = pipeline(criterium)
-        for preprocessed_criterium in preprocessed_criteria:
-            exclusion_criteria_preprocessed.append(preprocessed_criterium)
+    exclusion_entities = [entity for entity in exclusion_entities if entity not in exclusion_entities_negated]
+    exclusion_entities.extend([entity[2:] for entity in inclusion_entities_negated])
 
-    #move negatives
+    # Remove duplicates
+    inclusion_entities = list(set(inclusion_entities))
+    exclusion_entities = list(set(exclusion_entities))
 
-    exclusion_criteria_unique = []
-    inclusion_criteria_unique = []
-
-    for criterium in inclusion_criteria_preprocessed:
-        if criterium.startswith("n_"):
-            exclusion_criteria_unique.append(criterium[2:])
-        else:
-            inclusion_criteria_unique.append(criterium)
-
-    for criterium in exclusion_criteria_preprocessed:
-        if criterium.startswith("n_"):
-            inclusion_criteria_unique.append(criterium[2:])
-        else:
-            exclusion_criteria_unique.append(criterium)
-
-    #remove duplicates
-
-    inclusion_criteria_unique = dict.fromkeys(inclusion_criteria_unique)
-    exclusion_criteria_unique = dict.fromkeys(exclusion_criteria_unique)
-
-    #remove duplicates across criteria
-
-    exclusion_criteria_final = []
-    inclusion_criteria_final = []
-
-    exclusion_criteria_final = [item for item in exclusion_criteria_unique if item not in inclusion_criteria_unique]
-    inclusion_criteria_final = [item for item in inclusion_criteria_unique if item not in exclusion_criteria_unique]
-
+    # Remove entities that are in both lists
+    inclusion_entities_final = [entity for entity in inclusion_entities if entity not in exclusion_entities]
+    exclusion_entities_final = [entity for entity in exclusion_entities if entity not in inclusion_entities]
 
     preprocessed_trial = {"nct_id": trial['nct_id'],
                     "minimum_age": trial['minimum_age'],
                     "maximium_age": trial['maximium_age'],
                     "gender": trial['gender'],
-                    "inclusion": inclusion_criteria_final,
-                    "exclusion": exclusion_criteria_final}
+                    "inclusion": inclusion_entities_final,
+                    "exclusion": exclusion_entities_final}
 
-    with open("trials_preprocessed.json", "a") as file:
-        file.write(json.dumps(preprocessed_trial))
+    preprocessed_trials.append(preprocessed_trial)
 
-        if not index == len(trials)-1:
-            file.write(",")
-
-with open("trials_preprocessed.json", "a") as file:
-    file.write("]")
+with open(PREPROCESSED_TRIALS_TARGET, "w") as file:
+    json.dump(preprocessed_trials, file, indent=4)
